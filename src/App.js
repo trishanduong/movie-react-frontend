ce86a3900747aaeb030fb354921ab03b012583ea
trisha <trishanduong@gmail.com>
2024-02-05 15:18:24 -0800

    Added dependencies and resolved CORS issues, successfully fetching movie data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import api from "./api/axiosConfig";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data); 
+      setMovies(response.body);
+
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies(); 
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
